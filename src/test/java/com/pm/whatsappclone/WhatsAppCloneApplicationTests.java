71a475e95406c33f16fe894fec296c67fdd5311c
Tam107 <oicai107@gmail.com>
2025-06-30 23:10:44 +0700

    first initialize

---
+++ src/test/java/com/pm/whatsappclone/WhatsAppCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.whatsappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
